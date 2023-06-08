a8b609557a45a8ea0a2263e953ea5c5afcd1bb61
SimWooHyuk <tladngur2@naver.com>
2023-06-08 19:01:26 +0900

    나는 머리가 나쁘다

--- src/component/Template.jsx
+++ src/component/Template.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { styled } from 'styled-components';
-화면흐
+
 const TemplateWrapper = styled.div`
-  width: 1024px;
+  width: 200px;
   margin: 0 auto;
   margin-top: 6rem;
-  border-radius: 4px;
+  border-radius: 50%;
   overflow: hidden;
   cursor: pointer;
   .app-title {
     background: #3cb371;
     color: white;
-    height: 6rem;
+    height: 200px;
     font-size: 3rem;
     display: flex;
     justify-content: center;
@@ -32,7 +32,7 @@ function Template(props) {
 
   return (
     <TemplateWrapper>
-      <div className='app-title'>일정 관리</div>
+      <div className='app-title'>메모</div>
       <div className='content'>{children}</div>
     </TemplateWrapper>
   );
