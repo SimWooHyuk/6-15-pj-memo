040381e6e24b7c56ba51ce494a0b847ef461deee
SimWooHyuk <tladngur2@naver.com>
2023-06-13 17:37:01 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import React from 'react';
-import MemoJang from './component/NotePad';
+import NotePad from './component/NotePad';
 
 const App = () => {
 
   return (
     <div>
       {/* <h1>나의 메모장</h1> */}
-      <MemoJang />
+      <NotePad />
     </div>
   );
 };
 
+console.log(<NotePad />);
 export default App;
--- src/component/NotePad.jsx
+++ src/component/NotePad.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import styled from "styled-components";
 import { v4 as uuidv4 } from 'uuid';
 
@@ -8,9 +8,9 @@ const NotePadWrapper = styled.div`
   margin-top: 6rem;
   /* border-radius: 5px;
   border-color: #c93; */
-  border: 1px solid red;
+  border: 1px solid black;
   height: auto;
-
+  background-color: #f5dcb7;
   ul {
     list-style-type: none;
     padding: 0;
@@ -22,7 +22,7 @@ const NotePadWrapper = styled.div`
     height: 80px; 
     padding: 10px;
     font-size: 18px;
-    border-bottom: 1px solid #ccc;
+    border-bottom: 5px solid black;
     display: flex;
     align-items: center;
     justify-content: space-between; 
@@ -33,15 +33,15 @@ const NotePadWrapper = styled.div`
     height: 40px;
     padding: 10px;
     font-size: 24px;
-  border: 1px solid red;
+  border: 1px solid black;
 
   }
 
   .addButton {
     width: 76px;
     height: 30px;
-  border: 1px solid red;
-
+  border: 1px solid black;
+    cursor: pointer;
   }
 
   .deleteButton {
@@ -83,114 +83,156 @@ const NotePadWrapper = styled.div`
     width: 50%;
   }
   .searchBox {
-  border: 1px solid red;
+  border: 1px solid black;
   height: 24px;
   padding-top: 3px;
   }
   .searchBox::placeholder, input::placeholder {
-    font-weight: 900;
-    color: #000;
+    font-weight: 600;
+    color: #777;
   }
   .head , input::placeholder {
     text-align: center ;
   }
+  .head {
+    font-size: 40px;
+    padding: 10px 0 10px;
+  }
+  .padding4px {
+    padding: 5px;
+  border: 1px solid black;
+
+  }
+  body {
+    background-color: yellow;
+  }
+  .headDiv {
+    background-color: white;
+  }
+  .headImg {
+    background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLmJQNF052AEPZ37spJm-gQtTCpGpmCDv9OA&usqp=CAU );
+    height: 70px;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
 `;
+document.body.style = 'background: beige ;';
 
-const MemoCount = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
-  const [search, setSearch] = useState('');
+  const MemoCount = () => {
+    const [notes, setNotes] = useState([]);
+    const [newNote, setNewNote] = useState('');
+    const [search, setSearch] = useState('');
+    
+      useEffect(() => {
+        const storageNotes = localStorage.getItem('notes');
+        if (storageNotes) {
+          setNotes(JSON.parse(storageNotes));
+        }
+      }, []);
+      
+      useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+      }, [notes]);
 
-  const inputChange = (e) => {
-    const { value } = e.target;
-    setSearch(value);
-  };
 
-  useEffect(() => {
-    const storageNotes = localStorage.getItem('notes');
-    if (storageNotes) {
-      setNotes(JSON.parse(storageNotes));
+    const inputChange = (e) => {
+      const { value } = e.target;
+      setSearch(value);
+    };
+
+    const addNote = (e) => {
+      e.preventDefault(); 
+      const checkNote = newNote.trim();
+      if (checkNote !== '') {
+        setNotes([checkNote, ...notes]);
+        setNewNote('');
+        
+      } else {
+        alert('내용을 입력해주세요.');
+      }
+    };
+
+    const deleteNote = (index) => {
+      const updatedNotes = [...notes];
+      updatedNotes.splice(index, 1);
+      setNotes(updatedNotes);
+    };
+
+    const moveNoteToTop = (index) => {
+      const updatedNotes = [...notes];
+      const [note] = updatedNotes.splice(index, 1);
+      updatedNotes.unshift(note);
+      setNotes(updatedNotes);
+    };
+
+    const moveNoteToDown = (index) => {
+      const updatedNotes = [...notes];
+      const [note] = updatedNotes.splice(index, 1);
+      updatedNotes.push(note);
+      setNotes(updatedNotes);
+    };
+
+    const filteredNotes = notes.filter((note) => note.includes(search));
+    
+    function orderMemoUp() {
+      let newMemo = [...notes].sort()
+      setNotes(newMemo)
     }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('notes', JSON.stringify(notes));
-  }, [notes]);
-
-  const addNote = (e) => {
-    e.preventDefault(); // Prevent the form from submitting and refreshing the page
-
-    const checkNote = newNote.trim();
-    if (checkNote !== '') {
-      setNotes([checkNote, ...notes]);
-      setNewNote('');
-    } else {
-      alert('내용을 입력해주세요.');
+    function orderMemoDown() {
+      let newMemo = [...notes].sort().reverse();
+      setNotes(newMemo)
     }
+
+    const deleteAllNotes = () => {
+      setNotes([]);
+    };
+    
+    
+    return (
+      <NotePadWrapper>
+        <div className='headImg'></div>
+        <div className='head'>메모장</div>
+        
+        <form onSubmit={addNote}>
+          <div>
+            <input
+              className="input"
+              type="text"
+              value={newNote}
+              onChange={(e) => setNewNote(e.target.value)}
+              placeholder='추가할 메모를 작성 후 엔터'
+            />
+            <button className="addButton"  type="submit">
+              추가
+            </button>
+            <input type="text" className='searchBox' value={search} placeholder='검색할 내용을 입력하세요' onChange={inputChange} />
+            <button type='button' className='padding4px' onClick={orderMemoUp}>이름↑</button> 
+            <button type='button' className='padding4px' onClick={orderMemoDown}>이름↓</button> 
+            <button type='button' className='padding4px' onClick={deleteAllNotes}>메모 전체 삭제</button>
+            <p> {notes.length} 개</p>
+          </div>
+        </form>
+        <ul>
+          {filteredNotes.map((note, index) => (
+            <li key={uuidv4()} className="insert">
+              <div className="note-content">{note}</div>
+              <div className="button-container">
+                <button className="deleteButton" onClick={() => deleteNote(index)}>
+                  삭제
+                </button>
+                <button className="moveButton" onClick={() => moveNoteToTop(index)}>
+                  제일 위로
+                  <div className="date">[<span>{new Date().toLocaleDateString()}</span>]</div>
+                </button>
+                <button className="moveButton" onClick={() => moveNoteToDown(index)}>
+                  제일 아래로
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </NotePadWrapper>
+    );
   };
 
-  const deleteNote = (index) => {
-    const updatedNotes = [...notes];
-    updatedNotes.splice(index, 1);
-    setNotes(updatedNotes);
-  };
-
-  const moveNoteToTop = (index) => {
-    const updatedNotes = [...notes];
-    const [note] = updatedNotes.splice(index, 1);
-    updatedNotes.unshift(note);
-    setNotes(updatedNotes);
-  };
-
-  const moveNoteToDown = (index) => {
-    const updatedNotes = [...notes];
-    const [note] = updatedNotes.splice(index, 1);
-    updatedNotes.push(note);
-    setNotes(updatedNotes);
-  };
-
-  const filteredNotes = notes.filter((note) => note.includes(search));
-  
-  return (
-    <NotePadWrapper>
-      <h2 className='head'>메모장</h2>
-      <form onSubmit={addNote}>
-        <div>
-          <input
-            className="input"
-            type="text"
-            value={newNote}
-            onChange={(e) => setNewNote(e.target.value)}
-            placeholder='추가할 메모를 작성해주세요'
-          />
-          <button className="addButton"  type="submit">
-            추가
-          </button>
-          <input type="text" className='searchBox' value={search} placeholder='검색할 내용을 입력하세요' onChange={inputChange} /> 
-          <p>메모 개수: {notes.length}</p>
-        </div>
-      </form>
-      <ul>
-        {filteredNotes.map((note, index) => (
-          <li key={uuidv4()} className="insert">
-            <div className="note-content">{note}</div>
-            <div className="button-container">
-              <button className="deleteButton" onClick={() => deleteNote(index)}>
-                삭제
-              </button>
-              <button className="moveButton" onClick={() => moveNoteToTop(index)}>
-                제일 위로
-                <div className="date">[<span>{new Date().toLocaleDateString()}</span>]</div>
-              </button>
-              <button className="moveButton" onClick={() => moveNoteToDown(index)}>
-                제일 아래로
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </NotePadWrapper>
-  );
-};
-
-export default MemoCount;
+  export default React.memo(MemoCount);
